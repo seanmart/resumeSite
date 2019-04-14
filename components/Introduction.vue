aff01748f4864e329e2200091feb240a4c2869c2
Sean Metcalf <seanmart08@gmail.com>
2019-04-14 03:52:35 -0400

    updating introduction

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -31,12 +31,13 @@ section {
 
 .text {
   flex: 0 0 auto;
+  max-width: 500px;
 }
 
 .image {
   flex: 0 0 auto;
   padding: 30%;
-  margin: 40px 0px;
+  margin: 0px 0px 40px;
   border-radius: 100%;
   background-size: contain;
   background-repeat: no-repeat;
@@ -44,9 +45,18 @@ section {
   box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.1);
 }
 
+@media screen and (min-width: 700px) {
+  .image {
+    padding: 15%;
+  }
+}
+
 @media screen and (orientation: landscape) and (min-width: 800px) {
   section {
     flex-direction: row;
+    max-height: 1000px;
+    height: 100vh;
+    min-height: auto;
   }
 
   .text {
@@ -57,6 +67,7 @@ section {
   .image {
     flex: 0 0 60%;
     align-self: stretch;
+    margin-top: 40px;
     margin-right: 20px;
     margin-bottom: 0px;
     border-radius: 0px;

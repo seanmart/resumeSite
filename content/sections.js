0610a39e6f6b9447453344f4ad6b3456917fedd1
Sean Metcalf <seanmart08@gmail.com>
2019-04-14 21:16:14 -0400

    updating introduction

--- content/sections.js
+++ content/sections.js
@@ -1,6 +1,6 @@
 import introduction from './introduction.json'
 import videos from './videos.json'
-import highlights from './highlights.json'
+import keySkills from './keySkills.json'
 import contact from './contact.json'
 
-export default { introduction, videos, highlights, contact }
+export default { introduction, videos, keySkills, contact }

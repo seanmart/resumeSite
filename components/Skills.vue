be79ad90d332bd491e5606d212b3fada1e8ee82e
Sean Metcalf <smetcalf@toddstreet.com>
2019-04-16 15:53:18 -0400

    added featured and redesigned sizing and offset

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,6 +1,11 @@
 <template lang="html">
   <section :id="data.id" class="padding">
     <h1>{{ data.sectionName }}</h1>
+    <div class="container">
+      <template v-for="skill in data.skills">
+        <div class="skill"></div>
+      </template>
+    </div>
   </section>
 </template>
 

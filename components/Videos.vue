5e7ba777309659fb530d27a357b931c656fb114d
Sean Metcalf <smetcalf@toddstreet.com>
2019-04-18 14:52:37 -0400

    adding video modal iframe

--- components/Videos.vue
+++ components/Videos.vue
@@ -3,18 +3,14 @@
     <h1 class="padding top bottom">{{ data.sectionName }}</h1>
     <div class="container">
       <template v-for="video in data.list">
-        <div class="video-container" @click="selectVideo(video)">
+        <div class="video-container" v-if="video.link">
           <div class="video-ratio video">
-            <svg
-              class="play"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 70.35 79.8"
-            >
-              <path
-                d="M0,39.9V4.66a4.65,4.65,0,0,1,7-4L37.5,18.25,68,35.87a4.66,4.66,0,0,1,0,8.06L37.5,61.55,7,79.17a4.65,4.65,0,0,1-7-4Z"
-              />
-            </svg>
-            <div class="poster" :style="img(video.poster)" />
+            <iframe
+              :src="`${video.link}`"
+              frameborder="0"
+              allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
       </template>
@@ -63,26 +59,12 @@ section {
   transform: scale(0.95);
 }
 
-.poster {
-  z-index: 0;
-  opacity: 0.9;
+.video iframe {
   position: absolute;
   top: 0px;
   left: 0px;
-  bottom: 0px;
-  right: 0px;
-  background-size: cover;
-}
-
-.play {
-  z-index: 1;
-  left: 50%;
-  top: 50%;
-  width: 50px;
-  transform: translate(-50%, -50%);
-  position: absolute;
-  fill: #fff;
-  opacity: 1;
+  width: 100%;
+  height: 100%;
 }
 
 @media screen and (min-width: 800px) {
